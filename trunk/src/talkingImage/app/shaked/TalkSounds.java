6eb1951e43fd23a8dc4b52504bf8b324d2479bb6
shakedav@gmail.com <shakedav@gmail.com@3f248487-45d8-2444-3fe2-8de4a5973ddf>
2012-02-04 23:18:45 +0000

    Starting to do more sounds

--- trunk/src/talkingImage/app/shaked/TalkSounds.java
+++ trunk/src/talkingImage/app/shaked/TalkSounds.java
@@ -6,32 +6,31 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 
 public class TalkSounds {
-	private  SoundPool soundPool;
-	private  HashMap soundPoolHashMap;
+        private  SoundPool soundPool;
+        private  HashMap soundPoolHashMap;
 
-	public TalkSounds() {
-		soundPool = new SoundPool(4, AudioManager.STREAM_MUSIC, 0);
-		soundPoolHashMap = new HashMap();
-	}
+        public TalkSounds() {
+                soundPool = new SoundPool(4, AudioManager.STREAM_MUSIC, 0);
+                soundPoolHashMap = new HashMap(10);
+        }
 
-	public void addSound(int index, int soundID, Context context) {
-		int soundPoolID = soundPool.load(context, soundID, 1);
-		soundPoolHashMap.put(index, soundPoolID);
-	}
+        public void addSound(int index, int soundID, Context context) {
+                int soundPoolID = soundPool.load(context, soundID, 1);
+                soundPoolHashMap.put(index, soundPoolID);
+        }
 
-	public void play(int index, boolean loop) {
-		if (!loop)
-			soundPool.play((Integer) soundPoolHashMap.get(index), 1, 1, 1, 0, 1f);
-		else
-			soundPool.play((Integer) soundPoolHashMap.get(index), 1, 1, 1, -1, 1f);
-	}
+        public void play(int index, boolean loop) {
+                if (!loop)
+                        soundPool.play((Integer) soundPoolHashMap.get(index), 1, 1, 1, 0, 1f);
+                else
+                        soundPool.play((Integer) soundPoolHashMap.get(index), 1, 1, 1, -1, 1f);
+        }
 
-	public void stop(int index) {
-		soundPool.stop((Integer) soundPoolHashMap.get(index));
-	}
+        public void stop(int index) {
+                soundPool.stop((Integer) soundPoolHashMap.get(index));
+        }
 
-	public void release() {
-		soundPool.release();
-	}
+        public void release() {
+                soundPool.release();
+        }
 }
-
--- trunk/src/talkingImage/app/shaked/TalkingImageActivity.java
+++ trunk/src/talkingImage/app/shaked/TalkingImageActivity.java
@@ -1,9 +1,14 @@
 package talkingImage.app.shaked;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Random;
 
 import android.R.raw;
+import android.R.string;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,7 +21,8 @@ public class TalkingImageActivity extends Activity
 	protected Button Dad;
 	protected Button Mom;
 	protected SoundPool Pool;
-
+	protected ArrayList<Integer> fileNames;
+	
     TalkSounds talkSounds = new TalkSounds();
     /** Called when the activity is first created. */
     @Override
@@ -27,23 +33,44 @@ public class TalkingImageActivity extends Activity
         Mom = (Button) findViewById(R.id.Mom);
         ImageView image = (ImageView)findViewById(R.id.Image);
         image.setScaleType(ScaleType.FIT_XY);
-        talkSounds.addSound(1, R.raw.dad, getBaseContext());
-		talkSounds.addSound(2, R.raw.mom, getBaseContext());
-		
+        
+        AddSoundsToHash();
+        
         Dad.setOnClickListener(this);
         Mom.setOnClickListener(this);
         }
+    
+    public void AddSoundsToHash(){    	 
+    	talkSounds.addSound(1, R.raw.dad1, getBaseContext());
+    	talkSounds.addSound(2, R.raw.dad2, getBaseContext());
+    	talkSounds.addSound(3, R.raw.dad3, getBaseContext());
+    	talkSounds.addSound(4, R.raw.dad4, getBaseContext());
+    	talkSounds.addSound(5, R.raw.dad5, getBaseContext());
+    	talkSounds.addSound(6, R.raw.mom1, getBaseContext());
+    	talkSounds.addSound(7, R.raw.mom2, getBaseContext());
+    	talkSounds.addSound(8, R.raw.mom3, getBaseContext());
+    	talkSounds.addSound(9, R.raw.mom4, getBaseContext());
+    	talkSounds.addSound(10,R.raw.mom5, getBaseContext());         
+    }
 
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
+		Random rand = new Random();			
+		int index;
 		if (v == Dad)
 		{
-			talkSounds.play(1, false);
+			index = rand.nextInt(6);
+			talkSounds.play(index, false);
 		}
 		else if (v == Mom)
 		{
-			talkSounds.play(2, false);
+			int maximum = 10;
+			int minimum = 6;
+			Random rn = new Random();
+			int range = maximum - minimum + 1;
+			index =  rn.nextInt(range) + minimum;
+			talkSounds.play(index, false);
 		}
 	}
 
