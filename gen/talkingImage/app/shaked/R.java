bc8170fdbe3097a4d2420f2c44d859bf756f9277
shakedav@gmail.com <shakedav@gmail.com@3f248487-45d8-2444-3fe2-8de4a5973ddf>
2012-02-04 23:18:45 +0000

    Starting to do more sounds

--- gen/talkingImage/app/shaked/R.java
+++ gen/talkingImage/app/shaked/R.java
@@ -23,8 +23,16 @@ public final class R {
         public static final int main=0x7f030000;
     }
     public static final class raw {
-        public static final int dad=0x7f040000;
-        public static final int mom=0x7f040001;
+        public static final int dad1=0x7f040000;
+        public static final int dad2=0x7f040001;
+        public static final int dad3=0x7f040002;
+        public static final int dad4=0x7f040003;
+        public static final int dad5=0x7f040004;
+        public static final int mom1=0x7f040005;
+        public static final int mom2=0x7f040006;
+        public static final int mom3=0x7f040007;
+        public static final int mom4=0x7f040008;
+        public static final int mom5=0x7f040009;
     }
     public static final class string {
         public static final int Dad=0x7f050000;
